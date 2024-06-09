dafd7b96796f867bcd45a5b0d889d20f0e31c17b
Rafael Villegas <rafael@tutator.net>
2024-06-09 00:02:50 -0400

    testaaa

--- electron/rollup.config.js
+++ electron/rollup.config.js
@@ -1,16 +1,12 @@
-import commonjs from 'rollup-plugin-commonjs';
-
 export default {
   input: 'electron/build/electron/src/index.js',
   output: [
     {
       file: 'electron/dist/plugin.js',
-      name: 'tcapacitorElectronFilesystem',
       format: 'cjs',
       sourcemap: true,
       inlineDynamicImports: true,
     },
   ],
   external: ['@capacitor/core'],
-  plugins: [commonjs()],
 };
